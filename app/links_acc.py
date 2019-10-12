7f9003d27c979d8a5b780dbedbead27a4f5f2c06
Lancelot <bincha.1997@gmail.com>
2019-10-12 21:20:55 +0300

    Added unique domain list

--- app/links_acc.py
+++ app/links_acc.py
@@ -1,26 +1,44 @@
 from flask import Flask, jsonify
-import redis
 from flask import make_response, abort
 from flask import request, make_response
 import time
 import json
 from urllib.parse import urlparse
+from flask import Flask
+import redis
 
 app = Flask(__name__)
-app.debug = True
+HOST = 'localhost'
+PORT = 6379
+DB = 0
+DEC_RES = True
 
-db = redis.Redis(host='localhost', port=6379, db=0, decode_responses=True)
+db = redis.Redis(
+    host=HOST,
+    port=PORT,
+    db=DB,
+    decode_responses=DEC_RES
+    )
 
 def url_parce(list_links):
-    print(list_links)
-
+    domains = []
     for link in list_links:
-        u = urlparse(link)
-        result = '{uri.netloc}'.format(uri=u)
-        return result
+        uri = urlparse(link)
+        if uri.netloc:
+            result = '{uri.netloc}'.format(uri=uri)
+        elif uri.path:
+            result = link
+        else:
+            result = None
+        domains.append(result)
+    return domains
 
-
-
+def unique_list(domains):
+    unique_list = []
+    for x in domains:
+        if x not in unique_list:
+            unique_list.append(x)
+    return unique_list
 
 @app.route('/api/v1.0/visited_links', methods=['POST'])
 def add_links():
@@ -39,21 +57,22 @@ def add_links():
 @app.route('/api/v1.0/visited_domains', methods=['GET'])
 def get_links():
     list_links = []
-    domain = []
+    domains = []
     date_from = request.args.get('from', None)
     date_to = request.args.get('to', None)
+
     list_keys = db.keys()
     for key in list_keys:
         if int(date_from) <= int(key) <= int(date_to):
             list_links.append(db.get(key))
 
-    print(list_links)
     for link in list_links:
         dict_link = json.loads(link)
-        domain.append(url_parce(dict_link['links']))
+        domains += url_parce(dict_link['links'])
+    unique_domains = unique_list(domains)
 
     res = {
-        'domain': '{}'.format(domain),
+        'domains': unique_domains,
         'status': 'ok',
     }
     return jsonify(res), 200
@@ -63,6 +82,9 @@ def get_links():
 def not_found(error):
     return make_response(jsonify({'error': 'Not found'}), 404)
 
+@app.errorhandler(400)
+def bad_request(error):
+    return make_response(jsonify({'error': 'Bad request'}), 400)
 
 @app.route('/del', methods=['GET'])
 def delete_all():
@@ -72,12 +94,6 @@ def delete_all():
     }
     return jsonify(res), 200
 
-@app.errorhandler(400)
-def bad_request(error):
-    return make_response(jsonify({'error': 'Bad request'}), 400)
-
 if __name__ == '__main__':
     app.run(debug=True)
 
-
-# 1570818868
